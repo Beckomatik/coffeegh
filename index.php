e14d9469b4b69bf75b7637354a46970d9ac95372
Alan Dauphin <alandauphin@hotmail.fr>
2022-02-16 14:47:26 +0100

    waiter bdd?

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo ('Hello World ! Ca va?');
+// echo ('Hello World ! Ca va? ok ok ')
+
+function bdd()
+{
+    try {
+        $bdd = new PDO("mysql:dbname=blog;host=localhost", "root", "");
+    } catch (PDOException $e) {
+        echo "Connexion impossible: " . $e->getMessage();
+    }
+
+    return $bdd;
+}
